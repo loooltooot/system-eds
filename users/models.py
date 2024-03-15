b86f95b9d4cf6c7ae0e9c7196a08917be1ef8c46
Emir Zakrewski <frenkchambers@gmail.com>
2024-03-15 20:16:53 +0300

    🛂: user model created

--- users/models.py
+++ users/models.py
@@ -1,13 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.utils import timezone
 
 # Create your models here.
 class UserManager(BaseUserManager):
+    def _create_user(self, phone, surname, name, password=None, **extra_fields):
+        if not phone:
+            raise ValueError('Введите корректный номер телефона')
+
+        extra_fields['email'] = self.normalize_email(extra_fields.get('email'))
+        user = self.model(phone=phone, surname=surname, name=name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
     def create_user(self, phone, surname, name, password=None, **extra_fields):
-        pass
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(phone, surname, name, password, **extra_fields)
 
     def create_superuser(self, phone, surname, name, password=None, **extra_fields):
-        pass
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(phone, surname, name, password, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
     surname = models.CharField('фамилия', max_length=128)
@@ -18,6 +34,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     photo = models.ImageField('фото профиля', max_length=128, blank=True)
     is_staff = models.BooleanField('имеет доступ к панели', default=False)
     is_active = models.BooleanField('активная учетная запись', default=True)
+    date_joined = models.DateTimeField('дата регистрации', default=timezone.now)
 
     USERNAME_FIELD = 'phone'
     EMAIL_FIELD = 'email'
