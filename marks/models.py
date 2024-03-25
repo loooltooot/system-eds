22ef7f3919acb267a1acf9d590dfde834ddabbc8
Emir Zakrewski <frenkchambers@gmail.com>
2024-03-25 22:02:32 +0300

    🗃️: added restrictions to fk fields

--- marks/models.py
+++ marks/models.py
@@ -28,7 +28,10 @@ class Subject(models.Model):
 class Appointment(models.Model):
     students_unit = models.ForeignKey(StudentsUnit, on_delete=models.PROTECT, verbose_name='группа/класс')
     subject = models.ForeignKey(Subject, on_delete=models.PROTECT, verbose_name='предмет')
-    teacher = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, verbose_name='преподаватель')
+    teacher = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT, 
+        verbose_name='преподаватель', limit_choices_to={'groups__name': 'Преподаватели'}
+    )
     pub_date = models.DateTimeField('дата назначения', auto_now_add=True)
     
     class Meta:
@@ -41,8 +44,14 @@ class Appointment(models.Model):
 class Mark(models.Model):
     value = models.PositiveSmallIntegerField('оценка')
     subject = models.ForeignKey(Subject, on_delete=models.PROTECT, verbose_name='предмет')
-    teacher = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='set_marks', verbose_name='учитель')
-    student = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='received_marks', verbose_name='студент')
+    teacher = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT, 
+        related_name='set_marks', verbose_name='учитель', limit_choices_to={'groups__name': 'Преподаватели'}
+    )
+    student = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT, 
+        related_name='received_marks', verbose_name='студент', limit_choices_to={'groups__name': 'Студенты'}
+    )
     pub_date = models.DateTimeField('дата оценивания', auto_now_add=True)
 
     class Meta:
