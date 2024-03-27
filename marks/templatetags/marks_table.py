6fec176cf3061cc8789be3bea2b40729766a00a9
Emir Zakrewski <frenkchambers@gmail.com>
2024-03-27 19:29:08 +0300

    🚧: marks table added

--- marks/templatetags/marks_table.py
+++ marks/templatetags/marks_table.py
@@ -4,8 +4,16 @@ from marks.models import Mark
 
 register = template.Library()
 
-@register.inclusion_tag('marks/templatetags/marks_table.html')
-def marks_table(student, subject):
-    marks = Mark.objects.filter(student=student, subject=subject)
-    dates = [mark.pub_date for mark in marks]
-    return {'marks': marks, 'dates': dates}
+@register.inclusion_tag('marks/templatetags/marks_table.html', takes_context=True)
+def marks_table(context, students_unit, subject):
+    marks = Mark.objects.filter(student__students_unit=students_unit, subject=subject)
+    dates = sorted(set([mark.pub_date for mark in marks]))
+    marks = {}
+
+    for student in students_unit.user_set.all():
+        marks[student] = Mark.objects.filter(student=student, subject=subject)
+
+    context['marks_map'] = marks
+    context['dates'] = dates
+
+    return context
