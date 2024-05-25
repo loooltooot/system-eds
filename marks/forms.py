9c8d54f9f179cfeeed422e27a4543b91d9bcc04f
Emir Zakrewski <frenkchambers@gmail.com>
2024-05-25 21:02:52 +0300

    🚧: show mark modal

--- marks/forms.py
+++ marks/forms.py
@@ -5,4 +5,4 @@ from .models import Mark
 class MarkForm(forms.ModelForm):
     class Meta:
         model = Mark
-        fields = ['value', 'subject', 'teacher', 'student', 'pub_date']
+        fields = ['value', 'subject', 'teacher', 'student', 'feedback', 'pub_date']
