fa4ee2199a7d3b1ee11f4d5bc04c3c900040b01b
Emir Zakrewski <frenkchambers@gmail.com>
2024-05-29 21:37:09 +0300

    favicon

--- system_eds/settings.py
+++ system_eds/settings.py
@@ -37,14 +37,14 @@ ALLOWED_HOSTS = getenv('ALLOWED_HOSTS').split(',')
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
+    'marks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
-    'marks',
     'django.forms',
 ]
 
